6b25810941edafcc511a9a8a4bb2d41098a6d0af
Luke Worth <luke.worth@bellroy.com>
2025-03-05 10:55:42 +1100

    chore: bump nixpkgs, nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bellroy Tech Team Haskell Trial";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
