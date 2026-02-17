3eb455af8a1990a6bb080f1893e4b3fcfc37583a
Luke Worth <luke.worth@bellroy.com>
2026-02-17 14:11:36 +1100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/9393af3b506fc38ff805cef7baf912e97d1f4ed3?narHash=sha256-TXTImfTZmOYnS%2BAemOXiM7WDaeniuRHSruw3k%2Bv%2BBCY%3D' (2025-12-07)
      → 'github:nixos/nixpkgs/7cdb2c9a4f4ec945cdd8348800b9205e5069b48f?narHash=sha256-FNEmjUiwVcbaMx%2BDLNAPyEZMdw4ASGvEqJaswcJVRDc%3D' (2026-02-16)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bellroy Tech Team Haskell Trial";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
