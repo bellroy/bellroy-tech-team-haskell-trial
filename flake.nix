7ffd94b84ae15a6bcc9115b56b1c69d2885d2765
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2026-06-09 19:06:44 -0300

    chore: update nixpkgs to 26.05-darwin and stackage snapshot to LTS 24.45

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bellroy Tech Team Haskell Trial";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-26.05-darwin";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
